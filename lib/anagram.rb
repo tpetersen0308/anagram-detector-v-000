39b605936913cb63e8144d6a2ca6967b407a7fb7
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-05 18:49:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(candidates)
+    candidates.find_all do |candidate|
+      (candidate.size == @word.size) && candidate.split("").all? { |letter| @word.split("").include?(letter)}
+    end
+
+  end
+
+end
