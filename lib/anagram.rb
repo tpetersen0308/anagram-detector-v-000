5f747608ff468891e31981e230a1a21dd5c0120b
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-05 18:54:40 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,8 @@ class Anagram
 
   def match(candidates)
     candidates.find_all do |candidate|
-      (candidate.size == @word.size) && @word.split("").all? { |letter| candidate.split("").include?(letter)}
+      #(candidate.size == @word.size) && @word.split("").all? { |letter| candidate.split("").include?(letter)}
+      candidate.split("").sort == @word.split("").sort
     end
 
   end
