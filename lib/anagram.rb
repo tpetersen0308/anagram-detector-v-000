c3c5dc4cf471ea6cf76770a539230072230e66ef
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-05 18:55:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(candidates)
+    candidates.find_all do |candidate|
+      #(candidate.size == @word.size) && @word.split("").all? { |letter| candidate.split("").include?(letter)}
+      candidate.split("").sort == @word.split("").sort
+    end
+
+  end
+
+end
